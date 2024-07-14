59725aea2724a5b82515e99887b72c6974197573
ahmedsaid67 <96987324+ahmedsaid67@users.noreply.github.com>
2024-07-14 15:42:01 +0300

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import Menu,MenuItem,Sliders,UrunKategori,UrunVitrin,Urunler,Image,\
-    SosyalMedya,References,HizliLinkler,Contact,Hakkimizda,BaslikGorsel,SMedya,Beden
+    References,HizliLinkler,Contact,Hakkimizda,BaslikGorsel,SMedya,Beden
 
 
 admin.site.register(Menu)
@@ -13,7 +13,6 @@ admin.site.register(UrunKategori)
 admin.site.register(UrunVitrin)
 admin.site.register(Urunler)
 admin.site.register(Image)
-admin.site.register(SosyalMedya)
 admin.site.register(References)
 admin.site.register(HizliLinkler)
 admin.site.register(Contact)
